c4e54dd68edcbd143205b824d7634af421f24d54
real-Cinfante <53799339+real-Cinfante@users.noreply.github.com>
2022-04-20 17:52:38 -0400

    all cops

--- Juego.rb
+++ Juego.rb
@@ -1,68 +1,58 @@
+# frozen_string_literal: true
+
 require './Tablero'
 require './Jugador'
 
 class Juego
-    attr_accessor :jugador1
-    attr_accessor :jugador2
-    attr_accessor :vivo
+  attr_accessor :jugador1, :jugador2, :vivo
 
-    def initialize(dif, jug1, jug2, mod)
-        @jugador1 = Jugador.new(dif, jug1)
-        @jugador2 = Jugador.new(dif, jug2)
-        @turno = @jugador1
-        @noturno = @jugador2
-        @vivo = true
-        @mod = mod
+  def initialize(dif, jug1, jug2, mod)
+    @jugador1 = Jugador.new(dif, jug1)
+    @jugador2 = Jugador.new(dif, jug2)
+    @turno = @jugador1
+    @noturno = @jugador2
+    @vivo = true
+    @mod = mod
+  end
+
+  def turno
+    puts "**********\nTurno de #{@turno.nombre}**********"
+    breaker = true
+    while breaker
+      if @mod == 2 && @turno == @jugador2
+        breaker = false if @turno.disparar_IA(@noturno)
+      elsif @turno.disparar(@noturno)
+        breaker = false
+      end
     end
-
-
-    def turno
-        puts "**********\nTurno de "+@turno.nombre+"**********"
-        breaker = true
-        while breaker
-            if @mod == 2 && @turno == @jugador2
-                if @turno.disparar_IA(@noturno)
-                    breaker = false
-                end
-            else
-                if @turno.disparar(@noturno)
-                    breaker = false
-                end
-            end
-        end
-        revisar_jugador
-        if self.vivo
-            @noturno.get_tablero
-            terminar_turno
-        end
+    revisar_jugador
+    if vivo
+      @noturno.get_tablero
+      terminar_turno
     end
+  end
 
-    def terminar_turno
-        if @turno == @jugador1
-            @turno = @jugador2
-            @noturno = @jugador1
-        else
-            @turno = @jugador1
-            @noturno = @jugador2
-        end
+  def terminar_turno
+    if @turno == @jugador1
+      @turno = @jugador2
+      @noturno = @jugador1
+    else
+      @turno = @jugador1
+      @noturno = @jugador2
     end
+  end
 
-
-    def revisar_jugador
-        h = 0
-        for b in @noturno.barcos
-            if !b.vivo
-                h += 1
-            end
-        end
-        if h == @noturno.cant_barcos
-            terminar_juego
-        end
+  def revisar_jugador
+    h = 0
+    @noturno.barcos.each do |b|
+      h += 1 unless b.vivo
     end
+    terminar_juego if h == @noturno.cant_barcos
+  end
 
-    def terminar_juego
-        puts "**********El juego a terminado**********"
-        puts "********El ganador es #{@turno.nombre}!********"
-        @vivo = false
-    end
+  def terminar_juego
+    puts '**********El juego a terminado**********'
+    puts "********El ganador es #{@turno.nombre}!********"
+    @vivo = false
+  end
 end
