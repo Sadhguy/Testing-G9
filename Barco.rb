06365d077d425f4e359d84c25aef707685a31b32
Sadhguy <sadiaz9@uc.cl>
2022-04-22 13:57:31 -0400

    AI fixed

--- Barco.rb
+++ Barco.rb
@@ -1,33 +1,26 @@
+# frozen_string_literal: true
+
 class Barco
-    
-    attr_accessor :largo
-    attr_accessor :casillas
-    attr_accessor :vivo
+  attr_accessor :largo, :casillas, :vivo
 
-    def initialize(largo)
-        @largo = largo
-        @vida = largo
-        @vivo = true
-        @casillas = []
-    end
+  def initialize(largo)
+    @largo = largo
+    @vida = largo
+    @vivo = true
+    @casillas = []
+  end
 
-    def recibir_disparo
-        puts "me dieron"
-        @vida -= 1
-        puts "vida #{@vida}"
-        if @vida == 0
-            @vivo = false
-            puts "**********BARCO HUNDIDO!**********"
-        end
+  def recibir_disparo
+    @vida -= 1
+    if @vida.zero?
+      @vivo = false
+      puts '**********BARCO HUNDIDO!**********'
     end
+  end
 
-    def revisar_disparo(fil, col)
-        for c in 0..(@largo-1)
-            puts "Disparo #{fil} Casillas #{@casillas[c][0]}"
-            puts "Disparo #{col} Casillas #{@casillas[c][1]}"
-            if (fil == @casillas[c][0]) && (col == @casillas[c][1])
-                recibir_disparo
-            end
-        end
+  def revisar_disparo(fil, col)
+    (0..(@largo - 1)).each do |c|
+      recibir_disparo if (fil == @casillas[c][0]) && (col == @casillas[c][1])
     end
+  end
 end
--- Tablero.rb
+++ Tablero.rb
@@ -1,123 +1,124 @@
+# frozen_string_literal: true
+
 class Tablero
-    attr_accessor :casillas
-    def initialize(lado)
-        @lado = lado
-        @letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",\
-                    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-        @casillas = crear_tablero
+  attr_accessor :casillas
+
+  def initialize(lado)
+    @lado = lado
+    @letras = %w[A B C D E F G H I J K L M
+                 N O P Q R S T U V W X Y Z]
+    @casillas = crear_tablero
+  end
+
+  def crear_tablero
+    tablero = []
+    cont = 0
+    let = @letras.take(@lado)
+    tablero.push(let)
+    (1..@lado).each do |_f|
+      fil = []
+      (0..(@lado - 1)).each do |c|
+        fil.push(cont) if c.zero?
+        fil.push(0)
+      end
+      cont += 1
+      tablero.push(fil)
     end
+    tablero
+  end
 
-    def crear_tablero
-        tablero = []
-        cont = 0
-        let = @letras.take(@lado)
-        tablero.push(let)
-        for f in 1..@lado
-            fil = []
-            for c in 0..(@lado-1)
-                if c == 0
-                    fil.push(cont)
-                end 
-                fil.push(0)
-            end
-            cont += 1
-            tablero.push(fil)
-        end
-        return tablero
+  def print_tablero
+    (0..@lado).each do |f|
+      p @casillas[f]
     end
+  end
 
-    def print_tablero
-        for f in 0..@lado
-            p @casillas[f]
-        end
+  def pintar_casilla(fil, col, ind)
+    case ind
+    when 1
+      @casillas[fil][col] = 'X'
+    when 2
+      @casillas[fil][col] = 'o'
+    when 3
+      @casillas[fil][col] = '#'
     end
+  end
 
+  def revisar_casilla(fil, col)
+    f = fil.to_i
+    if (@letras.include? col.upcase) && ((0..(@lado - 1)).include? f)
+      c = @letras.find_index(col.upcase)
+      case @casillas[f + 1][c + 1]
+      when 0
+        pintar_casilla(f + 1, c + 1, 2)
+        puts '**********AGUA!**********'
+        [true, [f + 1, c + 1]]
+      when '#'
+        pintar_casilla(f + 1, c + 1, 1)
+        puts '**********FUEGO!**********'
+        [true, [f + 1, c + 1]]
+      else
+        puts 'Casilla no disponible'
+        [false]
+      end
 
-    def pintar_casilla(f, c, i)
-        if i == 1
-            @casillas[f][c] = 'X'
-        elsif i == 2
-            @casillas[f][c] = 'o'
-        elsif i == 3
-            @casillas[f][c] = '#'
-        end
     end
+  end
 
-
-    def revisar_casilla(fil, col)
-        f = fil.to_i()
-        if (@letras.include? col.upcase) && ((0..(@lado-1)).include? f)
-            c = @letras.find_index(col.upcase)
-            if @casillas[f+1][c+1] == 0
-                self.pintar_casilla(f+1, c+1, 2)
-                puts "**********AGUA!**********"
-                return [true, [f+1, c+1]]
-            elsif @casillas[f+1][c+1] == '#'
-                self.pintar_casilla(f+1, c+1, 1)
-                puts "**********FUEGO!**********"
-                return [true, [f+1, c+1]]
-            else
-                puts "Casilla no disponible"
-                return [false]
-            end
-
-        end
+  def revisar_para_barcos(col1, fil1, col2, fil2, largo)
+    fil1 = fil1.to_i
+    fil2 = fil2.to_i
+    col1 = @letras.find_index(col1.upcase)
+    col2 = @letras.find_index(col2.upcase)
+    tablero_provisional = self
+    if !((0..(@lado - 1)).include?  col1) || !((0..(@lado - 1)).include? fil1) || \
+       !((0..(@lado - 1)).include?  col2) || !((0..(@lado - 1)).include? fil2)
+      puts 'Casilla invalida'
+      return [false]
     end
-
-    def revisar_para_barcos(col1, fil1, col2, fil2, largo)
-        fil1 = fil1.to_i()
-        fil2 = fil2.to_i()
-        col1 = @letras.find_index(col1.upcase)
-        col2 = @letras.find_index(col2.upcase)
-        tablero_provisional = self
-        if !((0..(@lado-1)).include?  col1) || !((0..(@lado-1)).include? fil1) || \
-           !((0..(@lado-1)).include?  col2) || !((0..(@lado-1)).include? fil2) 
-            puts "Casilla invalida"
-            return [false]
-        end
-        if col1 == col2
-            fils = 0
-            cas = []
-            for i in (0..(largo-1))
-                if @casillas[fil1+1+i][col1+1] == 0
-                    fils += 1
-                    tablero_provisional.pintar_casilla(fil1+1+i, col1+1, 3)
-                    cas.push([fil1+1+i, col1+1])
-                else
-                    puts "No es posible colocar el barco en esa posición"
-                    return [false]
-                end
-            end
-            if fils == largo
-                @casillas = tablero_provisional.casillas
-                return [true, cas]
-            else
-                puts "No es posible colocar el barco en esa posición"
-                return [false]
-            end
-        elsif fil1 == fil2
-            cols = 0
-            cas = []
-            for i in (0..(largo-1))
-                if @casillas[fil1+1][col1+1+i] == 0
-                    cols += 1
-                    tablero_provisional.pintar_casilla(fil1+1, col1+1+i, 3)
-                    cas.push([fil1+1, col1+1+i])
-                else
-                    puts "No es posible colocar el barco en esa posición"
-                    return [false]
-                end
-            end
-            if cols == largo
-                @casillas = tablero_provisional.casillas
-                return [true, cas]
-            else
-                puts "No es posible colocar el barco en esa posición"
-                return [false]
-            end
+    if col1 == col2
+      fils = 0
+      cas = []
+      (0..(largo - 1)).each do |i|
+        if (@casillas[fil1 + 1 + i][col1 + 1]).zero?
+          fils += 1
+          tablero_provisional.pintar_casilla(fil1 + 1 + i, col1 + 1, 3)
+          cas.push([fil1 + 1 + i, col1 + 1])
         else
-            puts "No puedes colocar los barcos de forma diagonal"
-            return [false]
+          puts 'No es posible colocar el barco en esa posición'
+          return [false]
         end
+      end
+      if fils == largo
+        @casillas = tablero_provisional.casillas
+        [true, cas]
+      else
+        puts 'No es posible colocar el barco en esa posición'
+        [false]
+      end
+    elsif fil1 == fil2
+      cols = 0
+      cas = []
+      (0..(largo - 1)).each do |i|
+        if (@casillas[fil1 + 1][col1 + 1 + i]).zero?
+          cols += 1
+          tablero_provisional.pintar_casilla(fil1 + 1, col1 + 1 + i, 3)
+          cas.push([fil1 + 1, col1 + 1 + i])
+        else
+          puts 'No es posible colocar el barco en esa posición'
+          return [false]
+        end
+      end
+      if cols == largo
+        @casillas = tablero_provisional.casillas
+        [true, cas]
+      else
+        puts 'No es posible colocar el barco en esa posición'
+        [false]
+      end
+    else
+      puts 'No puedes colocar los barcos de forma diagonal'
+      [false]
     end
+  end
 end
